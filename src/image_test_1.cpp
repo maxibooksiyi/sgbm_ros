bc38ebe709a5e2b2cd09111b821d7b47cb6ce268
maxibooksiyi <1372740910@qq.com>
2024-03-12 11:34:23 +0800

    参考https://gitee.com/maxibooksiyi/awesome-sgbm/blob/maxitest/src/main_pnts.cpp目前改得可以读取两张D435i的灰度图并发出视差图的图像话题了，启动命令是rosrun offboard_pkg image_test_1
    参考https://gitee.com/maxibooksiyi/awesome-sgbm/blob/maxitest/src/main_pnts.cpp目前改得可以读取两张D435i的灰度图并发出视差图的图像话题了，启动命令是rosrun
    offboard_pkg image_test_1

--- src/image_test_1.cpp
+++ src/image_test_1.cpp
@@ -15,10 +15,36 @@ int main(int argc, char **argv)
 
     // 创建一个CvBridge对象
     cv_bridge::CvImage cv_image;
-    cv_image.encoding = "bgr8";
+    //cv_image.encoding = "bgr8";
+    
 
     // 读取图片
     cv::Mat img = cv::imread("/home/maxi/SGBM_ws/src/t265_to_mavros/img/test.jpg");
+
+   cv::Mat img_left = cv::imread("/home/maxi/SGBM_ws/src/t265_to_mavros/img/d435i/infra1_image.png",cv::IMREAD_GRAYSCALE);
+   cv::Mat img_right = cv::imread("/home/maxi/SGBM_ws/src/t265_to_mavros/img/d435i/infra2_image.png",cv::IMREAD_GRAYSCALE);
+
+   if (img_left.data == nullptr || img_right.data == nullptr) {
+        std::cout << "fail to read images" << std::endl;
+        return -1;
+   }
+   if (img_left.rows != img_right.rows || img_left.cols != img_right.cols) {
+        std::cout << "img_left.size not equals img_right" << std::endl;
+        return -1;
+   }
+
+   const sint32 width = static_cast<uint32>(img_left.cols);
+   const sint32 height = static_cast<uint32>(img_right.rows);
+
+   //the graydata of the left and right image
+   auto bytes_left = new uint8[width * height];
+   auto bytes_right = new uint8[width * height];
+   for (int i = 0; i < height; i++) {
+       for (int j = 0; j < width; j++) {
+            bytes_left[i * width + j] = img_left.at<uint8>(i, j);
+            bytes_right[i * width + j] = img_right.at<uint8>(i, j);
+       }
+   }
 
 
     // SGM匹配参数设计
@@ -49,9 +75,48 @@ int main(int argc, char **argv)
     // 定义SGM匹配类实例
     SemiGlobalMatching sgm;
 
+    // 初始化
+    if (!sgm.Initialize(width, height, sgm_option)) {
+        std::cout << "SGM初始化失败！" << std::endl;
+        return -2;
+    }
+    
+    // 匹配
+    auto disparity = new float32[uint32(width * height)]();
+    if (!sgm.Match(bytes_left, bytes_right, disparity)) {
+        std::cout << "SGM匹配失败！" << std::endl;
+        return -2;
+    }
+
+    // 显示视差图
+    cv::Mat disp_mat = cv::Mat(height, width, CV_8UC1);
+    float min_disp = width, max_disp = -width;
+    for (sint32 i = 0; i < height; i++) {
+        for (sint32 j = 0; j < width; j++) {
+            const float32 disp = disparity[i * width + j];
+            if (disp != Invalid_Float) {
+                min_disp = std::min(min_disp, disp);
+                max_disp = std::max(max_disp, disp);
+            }
+        }
+    }
+    for (sint32 i = 0; i < height; i++) {
+        for (sint32 j = 0; j < width; j++) {
+            const float32 disp = disparity[i * width + j];
+            if (disp == Invalid_Float) {
+                disp_mat.data[i * width + j] = 0;
+            }
+            else {
+                disp_mat.data[i * width + j] = static_cast<uchar>((disp - min_disp) / (max_disp - min_disp) * 255);
+            }
+        }
+    }
+
 
     // 转换图片格式为ROS消息
-    cv_image.image = img;
+    //cv_image.image = img;
+    cv_image.image = disp_mat;
+    cv_image.encoding = "mono8";  //灰度图这里注意用mono8
     sensor_msgs::ImagePtr img_msg = cv_image.toImageMsg();
 
     // 发布图像消息
