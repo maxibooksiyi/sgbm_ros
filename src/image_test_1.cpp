547a4d9a88bbe2bf00259f255f6b741ce8c8dff4
maxibooksiyi <1372740910@qq.com>
2024-03-12 11:21:01 +0800

    目前在image_test_1.cpp里面使用SemiGlobalMatching类可以编译通过，这才算是这个semiglobal移植得OK了，上次提交的虽然编译通过了，但是image_test_1.cpp里面使用SemiGlobalMatching类时编译还是有些问题，所以上次提交的cmakelists写得还是有些不对，似乎ROS功能包得cmakelists和单存c+=工程得cmakelists里面生成库得写法有点小区别？以后再细研究吧，目前这次提交的时编译没有问题的了。
    目前在image_test_1.cpp里面使用SemiGlobalMatching类可以编译通过，这才算是这个semiglobal移植得OK了，上次提交的虽然编译通过了，但是image_test_1.cpp里面使用SemiGlobalMatching类时编译还是有些问题，所以上次提交的cmakelists写得还是有些不对，似乎ROS功能包得cmakelists和单存c+=工程得cmakelists里面生成库得写法有点小区别？以后再细研究吧，目前这次提交的时编译没有问题的了。

--- src/image_test_1.cpp
+++ src/image_test_1.cpp
@@ -3,6 +3,8 @@
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/highgui/highgui.hpp"
 
+#include<SemiGlobalMatching.h>
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_publisher_node");
@@ -17,6 +19,36 @@ int main(int argc, char **argv)
 
     // 读取图片
     cv::Mat img = cv::imread("/home/maxi/SGBM_ws/src/t265_to_mavros/img/test.jpg");
+
+
+    // SGM匹配参数设计
+    SemiGlobalMatching::SGMOption sgm_option;
+    // 聚合路径数
+    sgm_option.num_paths = 8;
+    // 候选视差范围
+    sgm_option.min_disparity = 0;
+    sgm_option.max_disparity = 128;
+
+    // census窗口类型
+    sgm_option.census_size = SemiGlobalMatching::Census5x5;
+    // 一致性检查
+    sgm_option.is_check_lr = true;
+    sgm_option.lrcheck_thres = 1.0f;
+    // 唯一性约束
+    sgm_option.is_check_unique = true;
+    sgm_option.uniqueness_ratio = 0.99;
+    // 剔除小连通区
+    sgm_option.is_remove_speckles = true;
+    sgm_option.min_speckle_aera = 50;
+    // 惩罚项P1、P2
+    sgm_option.p1 = 10;
+    sgm_option.p2_init = 150;
+    // 视差图填充
+    sgm_option.is_fill_holes = false;
+
+    // 定义SGM匹配类实例
+    SemiGlobalMatching sgm;
+
 
     // 转换图片格式为ROS消息
     cv_image.image = img;
