979b07657856aa916d6c6da3946744c7930001d4
maxibooksiyi <1372740910@qq.com>
2024-03-12 12:50:19 +0800

    目前把image_test_1.cpp改得可以生成深度图并发出深度图话题了，注意发深度图话题时cv_image.encoding 需要赋值为16UC1，发出深度图话题后用rqt_image_view查看，也验证了我之前得推测，rqt_image_view显示深度图的时和会根据实际的深度图最大值对应缩放，进行显示，使得深度图的显示灰白层次明星，看着就和我之前用rqt_image_view保存的D435i的深度图看着差不多了，再次说明rqt_image_view不是按照原始深度值和65535的比例缩放为8位显示的，应该是按照原始深度值和最大深度值的比例缩放为8位灰度图显示的。
    目前把image_test_1.cpp改得可以生成深度图并发出深度图话题了，注意发深度图话题时cv_image.encoding
    需要赋值为16UC1，发出深度图话题后用rqt_image_view查看，也验证了我之前得推测，rqt_image_view显示深度图的时和会根据实际的深度图最大值对应缩放，进行显示，使得深度图的显示灰白层次明星，看着就和我之前用rqt_image_view保存的D435i的深度图看着差不多了，再次说明rqt_image_view不是按照原始深度值和65535的比例缩放为8位显示的，应该是按照原始深度值和最大深度值的比例缩放为8位灰度图显示的。

--- src/image_test_1.cpp
+++ src/image_test_1.cpp
@@ -5,6 +5,8 @@
 
 #include<SemiGlobalMatching.h>
 
+void disp2Depth(cv::Mat dispMap, cv::Mat &depthMap, cv::Mat K);
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_publisher_node");
@@ -113,10 +115,65 @@ int main(int argc, char **argv)
     }
 
 
+  //我自己加上深度图的生成
+    //left camera paras
+   double fx_1 = 442.05185684172238325;
+   double fy_1 = 442.05185684172238325;
+   double u_1 = 158.16831588745117188;
+   double v_1 = 216.55125045776367188;
+
+   cv::Mat K1 = (cv::Mat_<double>(3,3)<< fx_1, 0, u_1, 0, fy_1, v_1, 0, 0, 1);
+
+   //cv::Mat depth_mat = cv::Mat(height, width, CV_8UC1);
+   cv::Mat depth_mat = cv::Mat(height, width, CV_16UC1);  //深度图看了下都是16UC1，包括D435i的深度图
+   //cv::Mat depth_mat;
+   disp2Depth(disp_mat, depth_mat, K1);
+
+   uint16_t max_in_depth_mat = 0;
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+               int id = i*width + j;
+               //cout << "depth_mat.data " << (uint16_t)depth_mat.data[id] << endl;
+               //cout << "depthMap.at<uint16_t>(i, j) " << (uint16_t)depth_mat.at<uint16_t>(i, j) << endl;
+               //不能通过mat.data[id]的方式访问16位深度的深度图！！！！！它只能读取8位的值
+               //printf("depth_mat.data=%d\n", depth_mat.data[id]);
+               if( depth_mat.at<uint16_t>(i, j) > max_in_depth_mat )
+               {
+                  max_in_depth_mat = (uint16_t)depth_mat.at<uint16_t>(i, j) ;
+                  //cout << "depth_mat.data " << (uint16_t)depth_mat.at<uint16_t>(i, j)  << endl;
+               }
+
+            }
+        }
+    //cout << "max_in_depth_mat " << max_in_depth_mat << endl;
+
+   cv::Mat depth_mat_8U_for_show = cv::Mat(height, width, CV_8UC1);  //创建一个8位的灰度图用来显示16位的深度图
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int id = i*width + j;
+                //depthData[id] = ushort( (float)fx *baseline / ((float)dispData[id]) );
+                ///depthData[id] =  fx *baseline / ((int)dispMap.data[id]) ;
+                //depth_mat_8U_for_show.data[id] = ((int)depth_mat.data[id]/65535)*255;
+                //depth_mat_8U_for_show.data[id] = ((int)depth_mat.data[id]/max_in_depth_mat)*255;
+                depth_mat_8U_for_show.at<uint8_t>(i, j) = ( (float)depth_mat.at<uint16_t>(i, j)/max_in_depth_mat)*255;
+                //cout << "depth_mat_8U_for_show.at<uint16_t>(i, j) " << (uint16_t)depth_mat_8U_for_show.at<uint16_t>(i, j) << endl;
+            }
+        }
+
+    cv::imwrite("/home/maxi/SGBM_ws/src/t265_to_mavros/img/d435i/infra1_image_depth_maxi.png", depth_mat_8U_for_show);
+
+
     // 转换图片格式为ROS消息
     //cv_image.image = img;
-    cv_image.image = disp_mat;
-    cv_image.encoding = "mono8";  //灰度图这里注意用mono8
+    //cv_image.image = disp_mat;
+    cv_image.image = depth_mat;
+    //cv_image.encoding = "mono8";  //灰度图这里注意用mono8
+    //cv_image.encoding = "mono16";
+    cv_image.encoding = "16UC1";
     sensor_msgs::ImagePtr img_msg = cv_image.toImageMsg();
 
     // 发布图像消息
@@ -131,3 +188,71 @@ int main(int argc, char **argv)
     return 0;
 }
 
+
+
+
+/*
+函数作用：视差图转深度图
+参考自 https://blog.csdn.net/qq_36955294/article/details/84140788  
+输入：
+　　dispMap ----视差图，8位单通道，CV_8UC1
+　　K       ----内参矩阵，float类型
+输出：
+　　depthMap ----深度图，16位无符号单通道，CV_16UC1
+*/
+
+void disp2Depth(cv::Mat dispMap, cv::Mat &depthMap, cv::Mat K)
+{
+    int type = dispMap.type();
+
+    //float fx = K.at<float>(0, 0);  //这么读取的fx的值变为了2.03019e+24
+    double fx = K.at<double>(0, 0);  //现在找到之前自动读取fx值不对的原因了，double类型的变量用float类型去读取导致的
+    //float fx = 422;
+    //float fx = 382; //d435i
+    float fy = K.at<float>(1, 1);
+    float cx = K.at<float>(0, 2);
+    float cy = K.at<float>(1, 2);
+    //float baseline = 6500; //基线距离65mm
+    //float baseline = 80.0/camera_scale;
+    float baseline = 50; //D435i的双目基线是5厘米
+    //cout << "maxitest0" << endl;
+    if (type == CV_8U)
+    {
+        const float PI = 3.14159265358;
+        int height = dispMap.rows;
+        int width = dispMap.cols;
+
+        //uchar* dispData = (uchar*)dispMap.data;
+        uint8* dispData = (uint8*)dispMap.data;
+        ushort* depthData = (ushort*)depthMap.data;  //ushort关键字是System.UInt16的别名 
+        for (int i = 0; i < height; i++)
+        {
+            //cout << "maxitest1" << endl;
+            for (int j = 0; j < width; j++)
+            {
+                //cout << "maxitest2" << endl;
+                int id = i*width + j;
+                //cout << "id is" << id << endl;
+                //cout << "像素height坐标i is" << i << endl;
+                //cout << "像素width坐标j is" << j << endl;
+                if (!dispData[id])  continue;  //防止0除
+                //depthData[id] = ushort( (float)fx *baseline / ((float)dispData[id]) );
+                //depthData[id] =  fx *baseline / ((int)dispMap.data[id]) ;
+                //depthMap.data[id] = ushort( fx *baseline / ((int)dispMap.data[id]) );
+                depthMap.at<uint16_t>(i, j) = ushort( fx *baseline / ((int)dispMap.data[id]) );
+                //cout << "fx为" << fx << endl;
+                //cout << "baseline为" << baseline << endl;
+                //cout << "dispData[id]为" << dispData[id] << endl;
+                //cout << "dispMap.data[id]为" << (int)dispMap.data[id] << endl;
+                //cout << "算出的像素值也就是深度值(单位mm)为" << depthData[id] << endl;
+                //cout << "算出的像素值也就是深度值(单位mm)为" << (uint16_t)depthMap.data[id] << endl;
+                std::cout << "算出的像素值也就是深度值(单位mm)为" << (uint16_t)depthMap.at<uint16_t>(i, j) << std::endl;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "please confirm dispImg's type!" << std::endl;
+        //cv::waitKey(0);
+    }
+}
